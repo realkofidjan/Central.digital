c6064ece4a62b1bd3b11964f253c12e0a30a17e7
Nana Kofi Djan <83104183+realkofidjan@users.noreply.github.com>
2026-03-22 03:14:14 +0000

    Bug fix

---
+++ src/i18n.ts
@@ -0,0 +1,23 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+    resources: {
+      en: {
+        translation: {
+          // Add your English translations here
+          welcome: "Welcome to Central Digital"
+        }
+      }
+    }
+  });
+
+export default i18n;
